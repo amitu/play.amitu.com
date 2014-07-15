d10193beecd5c54cbf3ecc799674279f10cba492
Amit Upadhyay <amitu@browserstack.com>
2014-07-15 21:44:32 +0530

    using rice

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"net/http"
+	"github.com/GeertJohan/go.rice"
 )
 
 func main() {
+	fs := http.FileServer(rice.MustFindBox("public").HTTPBox())
+	http.Handle("/", fs)
 	http.ListenAndServe(":8877", &CORSMiddleware{http.DefaultServeMux})
 }
